e847fd814c48ef39faa43a048c899686278ed2f1
T47Y <smartlgtv683@gmail.com>
2025-04-02 10:52:13 +0530

    Add files via upload

---
+++ s_26_1.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+public class emp {
+public static void main(String args[]) {int empId = Integer.parseInt(args[0]);
+try {
+Class.forName("org.postgresql.Driver");
+Connection con =
+DriverManager.getConnection("jdbc:postgresql://localhost:5432/ty92","ty92","ty
+92");
+PreparedStatement pstmt = con.prepareStatement("DELETE FROM
+Employee WHERE eno = ?");
+pstmt.setInt(1, empId);
+int rowAffected = pstmt.executeUpdate();
+if(rowAffected > 0)
+System.out.println("\nDetails of employee with ID " + empId +
+" deleted successfully!");
+else
+System.out.println("Employee with ID " + empId + " not
+found.");
+} catch(SQLException | ClassNotFoundException e) {
+System.out.println(e);
+}
+}
+}
